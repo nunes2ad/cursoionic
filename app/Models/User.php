76407d7935c9b4aed7c2251285a866f964140f1b
nunesa2ad <cursoionic@a2ad.com.br>
2016-05-21 13:33:55 -0300

    corrigindo model de users que ficou com erro após gerar os repositories

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,10 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
-class User extends Model implements Transformable AuthenticatableContract,
+class User extends Model implements Transformable, AuthenticatableContract,
     AuthorizableContract,
     CanResetPasswordContract
 {
